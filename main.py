4ccd9865a6cadfb773df2c255698705a731d0fc7
Li caisong <licaisong0622@163.com>
2022-08-07 00:56:10 +0800

    123

--- main.py
+++ main.py
@@ -48,6 +48,6 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature,datetime = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature}, "datetime":{"value":datatime},  "love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"weather":{"value":wea},"temperature":{"value":temperature}, "datetime":{"value":datetime},  "love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
